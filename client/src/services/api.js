fa95eafe926d8cc427d2f3ce2da628c4ae6074af
Prashant Yadav <prashant@capricorn.online>
2026-02-16 12:18:58 +0100

    [Claude] [CODE MODE] Make direct code changes. Show complete file contents with changes. Still not registerat...

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const DEFAULT_API_URL = 'https://ranvijay.capricorn.online/prashant/api';
+const DEFAULT_API_URL = 'https://ranvijay.capricorn.online/api';
 const configuredApiUrl = process.env.REACT_APP_API_URL || DEFAULT_API_URL;
 
 const api = axios.create({
